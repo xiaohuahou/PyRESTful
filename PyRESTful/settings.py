f6bec4726059ef2ed0892f87f266bbc81870cc1a
xiaohua <houxiaohua@bigbaser.com>
2018-01-15 20:52:56 +0800

    [setting] set qa_mysql

--- PyRESTful/settings.py
+++ PyRESTful/settings.py
@@ -84,6 +84,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'qa_mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'Terminator666@',
+        'NAME': 'PyRESTful',
+        'HOST': '47.100.185.158',
+        'PORT': 3306
     }
 }
 
